173a79e1cebb506eecef872ade5803fd7a3cf1e1
umisetokikaze <umisetokikaze@gmail.com>
2022-10-14 18:09:24 +0900

    aaa

--- src/Manager/MG_status.py
+++ src/Manager/MG_status.py
@@ -5,7 +5,7 @@ class MGStatus:
         pass
 
     def InitStatus():
-        D = {"BaseData":{"name":"Base","HP":100,"RAM":100,"CLOCK":1,"ATK":10,"INT":10,"CONC":0,"DEF":10,"AGI":10,"LUK":10,"EXP":0,"LV":1}}
+        
         MD.PlayerDataRW("w",D)
 
     def StatusSet(StatusName, Status):
